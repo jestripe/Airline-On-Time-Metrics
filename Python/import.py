d12301ecdb21d8f746a842227bf870a6fbeaeaa4
Jesse <jesse.stripe@gmail.com>
2022-02-08 20:29:18 -0600

    Working import function.

--- Python/import.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import zipfile
-import psycopg2
-import sqlalchemy as sqla
-from sqlalchemy import create_engine
-
-psql_engine =create_engine('postgresql+psycopg2://postgres:PASSWORD@localhost/airlineontimemetrics')
-
-
---
+++ metrics_python/functions.py
@@ -0,0 +1,20 @@
+import os
+import pickle
+
+auth_path = rf'\home\jestripe\auth.pkl'
+
+def set_psql_auth(uid, pwd):
+    try:
+        with open(auth_path, 'rb') as f:
+            auth = pickle.load(f)
+    except (FileNotFoundError, EOFError):
+        auth = dict()
+
+    auth = (uid, pwd)
+    with open(auth_path, 'wb') as f:
+        pickle.dump(auth, f)
+
+def return_psql_auth():
+    with open(auth_path, 'rb') as f:
+        auth = pickle.load(f)
+    return auth
